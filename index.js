3e211c24bc3269746e461253040fbd2b1ad01fe2
aebeltran13 <aebeltran13@gmail.com>
2024-05-03 13:33:29 -0600

    Created file handling

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const dotenv = require('dotenv');
 dotenv.config();
 const { token } = process.env.DISCORD_TOKEN;
@@ -7,12 +9,57 @@ const { token } = process.env.DISCORD_TOKEN;
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+// Create a new Collection to store command files
+client.commands = new Collection();
+
+// Grab all the command files from the commands directory you created earlier
+const foldersPath = path.join(__dirmane, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+// Grab all the command files from the commands directory you created earlier
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filepath = path.join(commandsPath, file);
+		const command = require(filepath);
+		// Set a new item in the Collection with key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filepath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
 // When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
 // Log in to Discord with your client's token
 client.login(token);
+
+client.on(Events.InteractionCreate, async interaction => {
+	if(!interaction.isChatInputCommand()) return;
+
+	const command = intecation.client.commands.get(interaction.commandName);
+
+	if(!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+
+	console.log(interaction)
+});
